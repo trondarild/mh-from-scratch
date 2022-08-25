99113d97ef91e961597f84c2ca3149310c210662
trondarild <trondarild@gmail.com>
2022-08-25 10:35:14 +0200

    added sunspot ex

--- mh-from-scratch.jl
+++ mh-from-scratch.jl
@@ -6,6 +6,7 @@ using DataFrames
 using Underscores
 using StatsPlots
 using Query
+using CSV
 d = Normal(10, 3)
 mod1 = t -> rand(d, t)
 
@@ -59,22 +60,22 @@ function metropolis_hastings(
     # data: the data that we wish to model
     # acceptance_rule(x,x_new): decides whether to accept or reject the new sample 
     x = param_init
-    accepted = []
-    rejected = []
-    ret = DataFrame(t = Int[], accept = Int[], val=Float64[])
+    # accepted = []
+    # rejected = []
+    ret = DataFrame(t = Int[], accept = Int[], val=Vector{Vector{Float64}}())
     for i in 1:iterations
-        x_new = transition_model(x)
+        x_new = transition_model(x) # generate values to test for acceptance
         #println("mh xnew: " * string(x_new))
-        x_lik = likelihood_computer(x, data)
-        x_new_lik = likelihood_computer(x_new, data)
+        x_lik = likelihood_computer(x, data) # previous likelihood
+        x_new_lik = likelihood_computer(x_new, data) # new likelihood
         if(acceptance_rule(x_lik + log(prior(x)),
             x_new_lik + log(prior(x_new))))
             x = x_new
-            append!(accepted, x_new[2])
-            @_ [i, 1, x_new[2]] |> push!(ret, __)
+            # append!(accepted, x_new[2])
+            @_ [i, 1, x_new] |> push!(ret, __)
         else
-            append!(rejected, x_new[2])
-            @_ [i, 0, x_new[2]] |> push!(ret, __)
+            # append!(rejected, x_new[2])
+            @_ [i, 0, x_new] |> push!(ret, __)
         end
     end
     return ret #(accepted, rejected)
@@ -109,3 +110,40 @@ a = DataFrame(x=Int[], y=Float64[])
 @_ [2, 1.] |> push!(a, __)
 
 histogram(rand(Float64, 1000))
+
+
+
+#
+# sunspot example
+#
+
+# Transition model: 
+tr_mdl_ss = x -> [rand(Normal(x[1], 0.05)), rand(Normal(x[2], 0.5))]
+tr_mdl_ss([1, 2])
+
+function prior_ss(w)
+    if(w[1] <= 0 || w[2] <=0)
+        return 0
+    else
+        return 1
+    end
+end
+
+man_log_lik_gamma = (x, data) -> (x[1]-1).*log.(data) .- (1/x[2]).*data .- x[1]*log(x[2]) .- log(rand(Gamma(x[1]))) |> sum
+# read data
+
+data_ss = CSV.read("Catalogue_B.csv", DataFrame) 
+plot(data_ss[!, 1], data_ss[!, 2])
+
+ret_ss = metropolis_hastings(
+    man_log_lik_gamma,
+    prior_ss,
+    tr_mdl_ss,
+    [4, 10],
+    50000,
+    data_ss[!,3],
+    acceptance)
+
+histogram(data_ss[!, 2])
+#tst = DataFrame(t = Int[], accept = Int[], val=Vector{Vector{Float64}}())
+#@_ (1,1,[2., 1.]) |> push!(tst, __)
